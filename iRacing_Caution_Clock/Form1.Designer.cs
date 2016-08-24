1aad771a12febd985268779e287f9e47060899bc
Jordan M <Jordan M>
2016-08-23 21:07:53 -0400

    Initial Commit

---
+++ iRacing_Caution_Clock/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace iRacing_Caution_Clock
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConnectedToiRacing = new System.Windows.Forms.Label();
+            this.lblCurrentFlag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblConnectedToiRacing
+            // 
+            this.lblConnectedToiRacing.AutoSize = true;
+            this.lblConnectedToiRacing.Location = new System.Drawing.Point(12, 9);
+            this.lblConnectedToiRacing.Name = "lblConnectedToiRacing";
+            this.lblConnectedToiRacing.Size = new System.Drawing.Size(121, 13);
+            this.lblConnectedToiRacing.TabIndex = 0;
+            this.lblConnectedToiRacing.Text = "Connecting to iRacing...";
+            // 
+            // lblCurrentFlag
+            // 
+            this.lblCurrentFlag.AutoSize = true;
+            this.lblCurrentFlag.Location = new System.Drawing.Point(12, 66);
+            this.lblCurrentFlag.Name = "lblCurrentFlag";
+            this.lblCurrentFlag.Size = new System.Drawing.Size(70, 13);
+            this.lblCurrentFlag.TabIndex = 0;
+            this.lblCurrentFlag.Text = "Current Flag: ";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 283);
+            this.Controls.Add(this.lblCurrentFlag);
+            this.Controls.Add(this.lblConnectedToiRacing);
+            this.Name = "mainForm";
+            this.Text = "iRacing Caution Clock";
+            this.Leave += new System.EventHandler(this.mainForm_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConnectedToiRacing;
+        private System.Windows.Forms.Label lblCurrentFlag;
+    }
+}
+
---
+++ iRacing_Caution_Clock/Form1.cs
@@ -0,0 +1,66 @@
+﻿// This relies heavily on Nick Thissen's iRacing SDK Wrapper: https://github.com/NickThissen/iRacingSdkWrapper
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iRacing_Caution_Clock
+{
+    public partial class mainForm : Form
+    {
+        private readonly iRacingSdkWrapper.SdkWrapper wrapper;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            // create instance
+            wrapper = new iRacingSdkWrapper.SdkWrapper();
+
+            // listen to events
+            wrapper.TelemetryUpdated += OnTelemetryUpdated;
+            wrapper.SessionInfoUpdated += OnSessionInfoUpdated;
+
+            // start wrapper
+            wrapper.Start();
+        }
+
+        // do things when Telemetry updates (supposed to be 60 times per second)
+        private void OnTelemetryUpdated(object sender, iRacingSdkWrapper.SdkWrapper.TelemetryUpdatedEventArgs telemArgs)
+        {
+            Console.WriteLine("telemetry updated");
+
+            string currentFlag = telemArgs.TelemetryInfo.SessionFlags.Value.ToString();
+            // .Split('|')[0];
+
+            lblCurrentFlag.Text = "Current Flag: " + currentFlag;
+
+            if (wrapper.IsConnected)
+            {
+                lblConnectedToiRacing.Text = "Connected to iRacing!";
+            } else
+            {
+                lblConnectedToiRacing.Text = "Not Connected!";
+            }
+        }
+
+        // do things when session info updates
+        private void OnSessionInfoUpdated(object sender, iRacingSdkWrapper.SdkWrapper.SessionInfoUpdatedEventArgs SessionArgs)
+        {
+             // TODO: get leader and which lap he's on (need to know when we're 20 to go)
+
+            Console.WriteLine("session info updated");
+        }
+
+        private void mainForm_Leave(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
