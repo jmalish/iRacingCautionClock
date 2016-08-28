c8c69e53b4f8e3117f30045ca1399cbb64406c74
Jordan M <jordanmalish@gmail.com>
2016-08-27 22:04:40 -0400

    Add flag change recognition, add Admin check, add a whole lotta comments

--- iRacing_Caution_Clock/Form1.Designer.cs
+++ iRacing_Caution_Clock/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.lblConnectedToiRacing = new System.Windows.Forms.Label();
             this.lblCurrentFlag = new System.Windows.Forms.Label();
             this.lblCurrentLap = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblCautionClockStatus = new System.Windows.Forms.Label();
+            this.lblActualFlag = new System.Windows.Forms.Label();
+            this.lblCautionClockExpires = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblConnectedToiRacing
@@ -45,7 +49,7 @@
             // lblCurrentFlag
             // 
             this.lblCurrentFlag.AutoSize = true;
-            this.lblCurrentFlag.Location = new System.Drawing.Point(12, 66);
+            this.lblCurrentFlag.Location = new System.Drawing.Point(12, 38);
             this.lblCurrentFlag.Name = "lblCurrentFlag";
             this.lblCurrentFlag.Size = new System.Drawing.Size(73, 13);
             this.lblCurrentFlag.TabIndex = 0;
@@ -54,23 +58,63 @@
             // lblCurrentLap
             // 
             this.lblCurrentLap.AutoSize = true;
-            this.lblCurrentLap.Location = new System.Drawing.Point(12, 106);
+            this.lblCurrentLap.Location = new System.Drawing.Point(12, 78);
             this.lblCurrentLap.Name = "lblCurrentLap";
             this.lblCurrentLap.Size = new System.Drawing.Size(49, 13);
             this.lblCurrentLap.TabIndex = 0;
             this.lblCurrentLap.Text = "Lap - of -";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 105);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(33, 13);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Time:";
+            // 
+            // lblCautionClockStatus
+            // 
+            this.lblCautionClockStatus.AutoSize = true;
+            this.lblCautionClockStatus.Location = new System.Drawing.Point(12, 139);
+            this.lblCautionClockStatus.Name = "lblCautionClockStatus";
+            this.lblCautionClockStatus.Size = new System.Drawing.Size(99, 13);
+            this.lblCautionClockStatus.TabIndex = 0;
+            this.lblCautionClockStatus.Text = "Caution Clock: OFF";
+            // 
+            // lblActualFlag
+            // 
+            this.lblActualFlag.AutoSize = true;
+            this.lblActualFlag.Location = new System.Drawing.Point(12, 51);
+            this.lblActualFlag.Name = "lblActualFlag";
+            this.lblActualFlag.Size = new System.Drawing.Size(69, 13);
+            this.lblActualFlag.TabIndex = 0;
+            this.lblActualFlag.Text = "Actual Flag: -";
+            // 
+            // lblCautionClockExpires
+            // 
+            this.lblCautionClockExpires.AutoSize = true;
+            this.lblCautionClockExpires.Location = new System.Drawing.Point(126, 105);
+            this.lblCautionClockExpires.Name = "lblCautionClockExpires";
+            this.lblCautionClockExpires.Size = new System.Drawing.Size(33, 13);
+            this.lblCautionClockExpires.TabIndex = 0;
+            this.lblCautionClockExpires.Text = "Time:";
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 283);
+            this.Controls.Add(this.lblCautionClockExpires);
+            this.Controls.Add(this.lblTime);
             this.Controls.Add(this.lblCurrentLap);
+            this.Controls.Add(this.lblActualFlag);
             this.Controls.Add(this.lblCurrentFlag);
+            this.Controls.Add(this.lblCautionClockStatus);
             this.Controls.Add(this.lblConnectedToiRacing);
             this.Name = "mainForm";
             this.Text = "iRacing Caution Clock";
-            this.Leave += new System.EventHandler(this.mainForm_Leave);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +125,10 @@
         private System.Windows.Forms.Label lblConnectedToiRacing;
         private System.Windows.Forms.Label lblCurrentFlag;
         private System.Windows.Forms.Label lblCurrentLap;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblCautionClockStatus;
+        private System.Windows.Forms.Label lblActualFlag;
+        private System.Windows.Forms.Label lblCautionClockExpires;
     }
 }
 
--- iRacing_Caution_Clock/Form1.cs
+++ iRacing_Caution_Clock/Form1.cs
@@ -17,111 +17,163 @@ namespace iRacing_Caution_Clock
 {
     public partial class mainForm : Form
     {
-        private readonly iRacingSdkWrapper.SdkWrapper wrapper;
+        #region Variables
+        private readonly iRacingSdkWrapper.SdkWrapper wrapper;  // create sdk wrapper, used to connect to iRacing
 
-        private bool racing = false;
-        private bool twentyToGo = false;
-        private string currentFlag = "-";
-        private bool userIsAdmin = false;
+        private string currentFlag = "-";  // what the flag currently is (eg. green, caution, etc)
+        private bool userIsAdmin = false;  // is the user is admin or not, only admins can throw cautions
+        private int sessionTime = 0;  // what time it is in the session
+        private bool cautionClockActive = false;  // if the caution clock is running
+        private int cautionClockTime = 0;  // what time to throw the caution clock, uses the sessionTime
+        private int cautionClockTimeLength = 1200;  // how long between cautions, in seconds. Default is 1200
 
+        private bool closing = false;  // form was acting weird when closed, I think because of stuff trying to update after wrapper closed
+        #endregion
+
+        #region Form stuff
         public mainForm()
         {
-            InitializeComponent();
+            InitializeComponent();  // initialize the component, you know, that component
 
-            // create instance
-            wrapper = new iRacingSdkWrapper.SdkWrapper();
+            wrapper = new iRacingSdkWrapper.SdkWrapper();  // create wrapper instance
 
-            // listen to events
-            wrapper.TelemetryUpdated += OnTelemetryUpdated;
-            wrapper.SessionInfoUpdated += OnSessionInfoUpdated;
+            wrapper.TelemetryUpdated += OnTelemetryUpdated;  // listen to telemetry events
+            wrapper.SessionInfoUpdated += OnSessionInfoUpdated;  // listen to session events
 
-            // start wrapper
-            wrapper.Start();
+            wrapper.Start();  // start wrapper
         }
 
-        // do things when Telemetry updates (supposed to be 60 times per second)
-        private void OnTelemetryUpdated(object sender, iRacingSdkWrapper.SdkWrapper.TelemetryUpdatedEventArgs telemArgs)
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            currentFlag = telemArgs.TelemetryInfo.SessionFlags.Value.ToString().Split('|')[0]; // gets the current flag state and trims off the unneccessary stuff
+            wrapper.Stop();  // stop the wrapper since the form is being closed, otherwise we have a wild wrapper running free TODO: make a joke about debris cautions here
+        }
+        #endregion
 
-            lblCurrentFlag.Text = "Current Flag: " + currentFlag;
-
-            if (wrapper.IsConnected)
+        #region SDK Wrapper Stuff
+        private void OnTelemetryUpdated(object sender, iRacingSdkWrapper.SdkWrapper.TelemetryUpdatedEventArgs telemArgs)  // do things when Telemetry updates (supposed to be 60 times per second)
+        {
+            if (!closing)
             {
-                lblConnectedToiRacing.Text = "Connected to iRacing!";
-            } else
-            {
-                lblConnectedToiRacing.Text = "Not Connected!";
-            }
+                #region Flag stuff
+                string newFlag = telemArgs.TelemetryInfo.SessionFlags.Value.ToString().Split(' ')[0];  // get the actual current flag according to the session, splits because it normally shows two states
+
+                lblActualFlag.Text = newFlag;
+
+                if (currentFlag != newFlag)  // flag changed in the last update
+                {
+                    if (!newFlag.Contains("StartHidden"))  // don't want to show this flag to users, better to just tell them it's still green
+                    {
+                        currentFlag = newFlag;  // set current flag variable to the new flag
+
+                        if (newFlag.Contains("Caution")) // caution is out, starts out as "CautionWaving" and then turns to "Caution" after a short time
+                        {
+                            cautionClockActive = false; // turn off caution clock
+                        }
+                        else if (newFlag == "Green")
+                        {
+                            cautionClockActive = true; // turn on the caution clock
+                            cautionClockTime = sessionTime + cautionClockTimeLength; // set time for caution clock to expire
+
+                            lblCautionClockExpires.Text = (cautionClockTime - sessionTime).ToString();
+                        }
+
+                    }
+
+                    CheckCautionClock();
+                }
+
+                // currentFlag = telemArgs.TelemetryInfo.SessionFlags.Value.ToString().Split('|')[0]; // gets the current flag state and trims off the unneccessary stuff
+
+                lblCurrentFlag.Text = "Current Flag: " + currentFlag;
+                #endregion
+
+                #region Time stuff
+                string currentTime = telemArgs.TelemetryInfo.SessionTime.ToString().Split('.')[0];  // get the session time (how long it's been since this session started)
+                                                                                                    // splitting at decimal because we don't need to know the exact milisecond
+                sessionTime = Convert.ToInt32(currentTime);  // convert it to int so we can use it as a number
 
 
-
-            if (!racing || twentyToGo)
-            {
-                // turn off caution clock
+                lblTime.Text = "Time: " + sessionTime.ToString();  // update time label
+                #endregion
+                
+                if (wrapper.IsConnected) // check if wrapper has connected yet
+                {
+                    lblConnectedToiRacing.Text = "Connected to iRacing!";  // let user know we've connected to the wrapper
+                }
             }
         }
 
         // do things when session info updates
         private void OnSessionInfoUpdated(object sender, iRacingSdkWrapper.SdkWrapper.SessionInfoUpdatedEventArgs sessionArgs)
         {
-            if (sessionArgs.SessionInfo.IsValidYaml)  // check if yaml is valid
+            if (!closing)
             {
-                Deserializer deserializer = new Deserializer(namingConvention: new PascalCaseNamingConvention(), ignoreUnmatched: true);  // create a deserializer
-                var input = new StringReader(sessionArgs.SessionInfo.Yaml);  // read the yaml
-                var sessionInfo = deserializer.Deserialize<SDKReturn>(input);  // deserialize the yaml
-
-                #region Session Info
-                foreach (var session in sessionInfo.SessionInfo.Sessions)  // look through all the sessions (normally they're Practice, Qualifying, and Race)
+                if (sessionArgs.SessionInfo.IsValidYaml)  // check if yaml is valid
                 {
-                    if (session.SessionType == "Race") // find the race session
+                    Deserializer deserializer = new Deserializer(namingConvention: new PascalCaseNamingConvention(), ignoreUnmatched: true);  // create a deserializer
+                    var input = new StringReader(sessionArgs.SessionInfo.Yaml);  // read the yaml
+                    var sessionInfo = deserializer.Deserialize<SDKReturn>(input);  // deserialize the yaml
+
+                    #region Session Info
+                    foreach (var session in sessionInfo.SessionInfo.Sessions)  // look through all the sessions (normally they're Practice, Qualifying, and Race)
                     {
-                        racing = true; // set racing to true, so we know the race session has started
-                        var raceSession = session;  // the session we're dealing with is the race session, just using this to shorten the variable to less than 50 characters
-
-                        int lapsComplete = Convert.ToInt32(raceSession.ResultsLapsComplete);  // get the laps complete
-
-                        if (lapsComplete > 0) // if ResultsLapsComplete is -1, we haven't entered the race yet
+                        if (session.SessionType == "Race") // find the race session
                         {
-                            lapsComplete += 1; // laps are 0 indexed, need to add 1 to get it to display correctly
+                            var raceSession = session;  // the session we're dealing with is the race session, just using this to shorten the variable to less than 50 characters
+                            int lapsComplete = Convert.ToInt32(raceSession.ResultsLapsComplete);  // get the laps complete
 
-                            lblCurrentLap.Text = string.Format("Lap {0} of {1}", lapsComplete.ToString(), raceSession.SessionLaps);  // set label on form to show what lap we're on
-
-                            if ((Convert.ToInt32(session.SessionLaps)) - (Convert.ToInt32(lapsComplete)) <= 20)  // check if we're less than 20 to go in the race
+                            if (lapsComplete > 0) // if ResultsLapsComplete is -1, we haven't entered the race yet
                             {
-                                twentyToGo = true; // race is now twenty to go, caution clock needs to be turned off
+                                lapsComplete += 1; // laps are 0 indexed, need to add 1 to get it to display correctly
+                                lblCurrentLap.Text = string.Format("Lap {0} of {1}", lapsComplete.ToString(), raceSession.SessionLaps);  // set label on form to show what lap we're on
+
+                                if ((Convert.ToInt32(session.SessionLaps)) - (Convert.ToInt32(lapsComplete)) <= 20)  // check if we're less than 20 to go in the race
+                                {
+                                    // cautionClockActive = false;  // turn off caution clock
+                                }
                             }
                         }
                     }
-                }
-                #endregion
+                    #endregion
 
-                #region Radio Info
-                if (userIsAdmin)  // if this is true, we already know user is admin, no reason to keep checking every few seconds
-                {
-                    foreach (var radio in sessionInfo.RadioInfo.Radios)  // look through all the radios (normally only 1)
+                    #region Radio Info
+                    if (userIsAdmin)  // if this is true, we already know user is admin, no reason to keep checking every few seconds
                     {
-                        foreach (var freq in radio.Frequencies)  // look through all the frequencies
+                        foreach (var radio in sessionInfo.RadioInfo.Radios)  // look through all the radios (normally only 1)
                         {
-                            if (freq.FrequencyName.Contains("ADMIN"))  // check to see if there's a radio named ADMIN
+                            foreach (var freq in radio.Frequencies)  // look through all the frequencies
                             {
-                                userIsAdmin = true;  // if so, user is admin, so we know they can control cautions
+                                if (freq.FrequencyName.Contains("ADMIN"))  // check to see if there's a radio named ADMIN
+                                {
+                                    userIsAdmin = true;  // if so, user is admin, so we know they can control cautions
+                                }
                             }
                         }
                     }
+                    #endregion
                 }
-                #endregion
-            }
-            else {
-                Console.WriteLine("Not valid YAML");
+                else
+                {
+                    Console.WriteLine("Not valid YAML!!"); // this shouldn't ever really be reached, if so, there's something wrong with the wrapper
+                }
             }
         }
+        #endregion
 
-        private void mainForm_Leave(object sender, EventArgs e)
+        #region FUNctions
+        private void CheckCautionClock()  // function that pretty much contains the caution clock
         {
-            
-        }
+            if (cautionClockActive)  // first make sure caution clock is even active
+            {
+                lblCautionClockStatus.Text = "Caution Clock: ACTIVE";
+            } else
+            {
+                lblCautionClockStatus.Text = "Caution Clock: OFF";
+            }
 
+            return;
+        }
+        #endregion
 
         #region Classes for YAML Parsing
         public class SDKReturn
